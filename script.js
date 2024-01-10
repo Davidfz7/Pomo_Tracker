3a654531aba0a9877cd6e5128a0ff47047c99458
davidfz7 <david123.dd212@gmail.com>
2024-01-10 17:03:12 -0600

    allToMe

--- script.js
+++ script.js
@@ -25,11 +25,14 @@ var cells = table.getElementsByTagName("td");
 for(var i = 0; i< cells.length; i++){
    (function(index){
         cells[index].addEventListener("click", function() {
-            alert("Hello world");  
+            const dialogg = document.getElementById('cell-info-dialog');
+
+            dialogg.showModal();
+
         });
     })(i);
 }
-
+console.log(sessionStorage);
 // var dialogsTd = table.getElementsByTagName("dialog");
 // var listDialog = [];
 // for(var j = 0; j< dialogsTd.length; j++){ 
@@ -37,15 +40,16 @@ for(var i = 0; i< cells.length; i++){
 // }
 //console.log(dialogsTd);
 
-var colors = {green: ["rgb(0, 170, 0)", "rgb(0, 190, 0)",
-                      "rgb(0, 210, 0)", "rgb(0, 230, 0)",
-                      "rgb(0, 255, 0)"],
-              red: ["rgb(170, 0, 0)", "rgb(190, 0, 0)",
-                    "rgb(210, 0, 0)", "rgb(230, 0, 0)",
-                    "rgb(255, 0, 0)"]};
+var colorScale = document.getElementById("color-scale");
 
-localStorage.setItem("colors", JSON.stringify(colors));
 
-var recoverColors = JSON.parse(localStorage.getItem('colors'));
-
-console.log(recoverColors);
+var colors = ["rgb(0, 170, 0)", "rgb(0, 190, 0)", "rgb(0, 210, 0)",
+             "rgb(0, 230, 0)", "rgb(0, 255, 0)", "rgb(255, 0, 0)",
+             "rgb(230, 0, 0)", "rgb(210, 0, 0)", "rgb(190, 0, 0)",
+             "rgb(170, 0, 0)"];
+for(var i = 0; i < colors.length; i++){
+    var newDiv = document.createElement("div");
+    newDiv.style.backgroundColor = colors[i];
+    newDiv.className = "color-box";
+    colorScale.appendChild(newDiv);
+}
